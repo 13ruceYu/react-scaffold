c61b920a411e6fdedb097673ee1fc52c98144790
13ruceYu <bruceyuhongbo@gmail.com>
2021-04-11 22:32:05 +0800

    feat: practice react todo app

---
+++ src/components/TodoApp/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class TodoApp extends Component {
+  render() {
+    return (
+      <div className="todo-app">
+        todo app
+      </div>
+    );
+  }
+}
+
+export default TodoApp;
--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class Test extends Component {
+export default class TodoList extends Component {
   render() {
     return (
       <div>
-        
+        TodoList
       </div>
     )
   }
