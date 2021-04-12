2ad178831af6189e64a24d8b8915edb250701315
yuhongbo <953127874@qq.com>
2021-04-12 08:49:07 +0800

    local commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 // 注意！这个不是结构赋值，而是分别暴露。
 import React, {Component} from 'react'
-import Hello from './components/Hello/Hello'
-import Welcome from './components/Welcome'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <Hello></Hello>
-        <Welcome></Welcome>
       </div>
     )
   }
