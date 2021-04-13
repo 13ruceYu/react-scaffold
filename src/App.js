d133498a92b57da38d6f832507b8c183b6d076e0
13ruceYu <bruceyuhongbo@gmail.com>
2021-04-13 17:27:59 +0800

    feat: react proxy setting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 // 注意！这个不是结构赋值，而是分别暴露。
-import React, {Component} from 'react'
-import TodoApp from './components/TodoApp'
+import React, { Component } from 'react'
+import axios from 'axios'
 import './global.css'
 
 export default class App extends Component {
+
+  getData = () => {
+    axios.get('http://localhost:3000/api1/students').then(
+      r => {
+        console.log(r.data);
+      },
+      e => {
+        console.log(e);
+      }
+    )
+  }
+
+  getCarData = () => {
+    axios.get('http://localhost:3000/api2/cars').then(
+      r => {
+        console.log(r.data);
+      },
+      e => {
+        console.log(e);
+      }
+    )
+  }
+
   render () {
     return (
       <div>
-        <TodoApp></TodoApp>
+        <button onClick={this.getData}>get data</button>
+        <button onClick={this.getCarData}>get car data</button>
       </div>
     )
   }
