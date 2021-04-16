f2841a1a7cdbc5c21b9056a3703efa2efe2da099
13ruceYu <bruceyuhongbo@gmail.com>
2021-04-16 09:39:17 +0800

    init src

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 // 注意！这个不是结构赋值，而是分别暴露。
 import React, { Component } from 'react'
-import GithubSearchApp from './components/GithubSearchApp'
 import './global.css'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <GithubSearchApp />
+        app
       </div>
     )
   }
